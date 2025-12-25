04d3c7f9d114e3b9841457108eab6527cf07ebb2
RVisalD <salduong97@gmail.com>
2025-12-25 21:58:03 +0700

    Initial Commit

---
+++ Product/ProductApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProductApp.swift
+//  Product
+//
+//  Created by RatanakVisal on 25/12/25.
+//
+
+import SwiftUI
+
+@main
+struct ProductApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
